7c64c87d51bf19c9d140d16748f53fb2bef45483
nicius2 <marco.marks06@gmail.com>
2026-01-14 00:54:44 -0300

    feat: adiciona protecao de rotas RBAC

--- backend-node-vix-test/src/routes/brandMaster.routes.ts
+++ backend-node-vix-test/src/routes/brandMaster.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { BrandMasterController } from "../controllers/BrandMasterController";
 import { API_VERSION, ROOT_PATH } from "../constants/basePathRoutes";
-// import { isManagerOrIsAdmin } from "../auth/isManagerOrIsAdmin";
-// import { isAdmin } from "../auth/isAdmin";
-// import { authUser } from "../auth/authUser";
+import { isManagerOrIsAdmin } from "../auth/isManagerOrIsAdmin";
+import { isAdmin } from "../auth/isAdmin";
+import { authUser } from "../auth/authUser";
 
 const BASE_PATH = API_VERSION.V1 + ROOT_PATH.BRANDMASTER; // /api/v1/brand-master
 
@@ -15,30 +15,26 @@ export const makeBrandMasterController = () => {
 
 const brandMasterController = makeBrandMasterController();
 
-brandMasterRoutes.get(`${BASE_PATH}/self`, async (req, res) => {
+brandMasterRoutes.get(`${BASE_PATH}/self`, authUser, async (req, res) => {
   await brandMasterController.getSelf(req, res);
 });
 
 brandMasterRoutes.get(
   `${BASE_PATH}/:idBrandMaster`,
-  // authUser
+  authUser,
   async (req, res) => {
     await brandMasterController.getById(req, res);
   },
 );
 
-brandMasterRoutes.get(
-  `${BASE_PATH}`,
-  // authUser
-  async (req, res) => {
-    await brandMasterController.listAll(req, res);
-  },
-);
+brandMasterRoutes.get(`${BASE_PATH}`, authUser, async (req, res) => {
+  await brandMasterController.listAll(req, res);
+});
 
 brandMasterRoutes.post(
   `${BASE_PATH}`,
-  // authUser,
-  // isManagerOrIsAdmin,
+  authUser,
+  isManagerOrIsAdmin,
   async (req, res) => {
     await brandMasterController.createNewBrandMaster(req, res);
   },
@@ -46,8 +42,8 @@ brandMasterRoutes.post(
 
 brandMasterRoutes.put(
   `${BASE_PATH}/:idBrandMaster`,
-  // authUser,
-  // isManagerOrIsAdmin,
+  authUser,
+  isManagerOrIsAdmin,
   async (req, res) => {
     await brandMasterController.updateBrandMaster(req, res);
   },
@@ -55,8 +51,8 @@ brandMasterRoutes.put(
 
 brandMasterRoutes.delete(
   `${BASE_PATH}/:idBrandMaster`,
-  // authUser,
-  // isAdmin,
+  authUser,
+  isAdmin,
   async (req, res) => {
     await brandMasterController.deleteBrandMaster(req, res);
   },
--- backend-node-vix-test/src/routes/uploads.routes.ts
+++ backend-node-vix-test/src/routes/uploads.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { API_VERSION, ROOT_PATH } from "../constants/basePathRoutes";
 import { BucketController } from "../controllers/BucketController";
 import { BucketLocalService } from "../services/BucketLocalService";
+import { authUser } from "../auth/authUser";
 
 const BASE_PATH = API_VERSION.V1 + ROOT_PATH.UPLOADS; // /api/v1/uploads
 
@@ -14,7 +15,7 @@ export const makeBucketController = () => {
 
 const uploadsController = makeBucketController();
 
-uploadsRoutes.get(`${BASE_PATH}/:objectName`, async (req, res) => {
+uploadsRoutes.get(`${BASE_PATH}/:objectName`, authUser, async (req, res) => {
   await uploadsController.getFileInBucketByObjectName(req, res);
 });
 
