25f128b91edd329c3aaae642526fdfed5dec1fbf
nicius2 <marco.marks06@gmail.com>
2026-01-16 09:57:27 -0300

    fix: corrige a tipagem do zod para com o prisma

--- backend-node-vix-test/src/types/validations/VM/createVM.ts
+++ backend-node-vix-test/src/types/validations/VM/createVM.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 const EVMStatus = z.enum(["RUNNING", "STOPPED", "PAUSED"]);
+const ETaskLocation = z.enum(["AZURE", "AWS", "GCP", "LOCAL"]);
+
 // Password validation regex
 export const passwordRegex = {
   numbers: /(?=.*\d.*\d)/,
@@ -12,6 +14,8 @@ export const passwordRegex = {
 
 export const vMCreatedSchema = z.object({
   vmName: z.string().optional(),
+  pass: z.string().min(1, "Password is required"),
+  location: ETaskLocation.optional(),
   vCPU: z.number().min(1, "vCPU must be at least 1"),
   ram: z.number().min(1, "RAM must be at least 1 GB"),
   disk: z.number().min(20, "Disk must be at least 20 GBs"),
